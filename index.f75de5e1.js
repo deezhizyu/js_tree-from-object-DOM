8db4b7d6942c0a5aab234530fe9de20f5008e5c9
deezhizyu <kalenskuja@gmail.com>
2024-06-09 22:27:47 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,39 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    let targetList = element;
+    if (!targetList) {
+        targetList = document.createElement("ul");
+        targetList.setAttribute("id", "tree");
+        document.body.append(targetList);
+    }
+    for (const [key, value] of Object.entries(data)){
+        const listItem = document.createElement("li");
+        listItem.textContent = key;
+        if (Object.values(value).length > 0) {
+            const nestedList = document.createElement("ul");
+            listItem.append(nestedList);
+            createTree(nestedList, value);
+        }
+        targetList.append(listItem);
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
